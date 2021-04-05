640b699b14c469c19f11076219da41c213f040fd
linxiao <1130037266@qq.com>
2021-04-05 19:01:56 +0800

    ext

--- app/src/main/java/com/lx/linwanandroid_mvvm/ext/Ext.kt
+++ app/src/main/java/com/lx/linwanandroid_mvvm/ext/Ext.kt
@@ -1,8 +1,13 @@
 package com.lx.linwanandroid_mvvm.ext
 
+import android.app.Activity
 import android.content.Context
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import com.lx.linwanandroid_mvvm.R
 
 /**
  * @title：Ext
@@ -17,4 +22,19 @@ fun Fragment.showToast(string: String){
 
 fun Context.showToast(string: String) {
     Toast.makeText(this, string, Toast.LENGTH_SHORT).show()
+}
+
+fun Activity.showSnackMsg(msg: String){
+    val snackbar = Snackbar.make(this.window.decorView, msg, Snackbar.LENGTH_SHORT)
+    val view = snackbar.view
+    view.findViewById<TextView>(R.id.snackbar_text).setTextColor(ContextCompat.getColor(this, R.color.white))
+    snackbar.show()
+}
+
+fun Fragment.showSnackMsg(msg: String) {
+    this.activity ?: return
+    val snackbar = Snackbar.make(this.requireActivity().window.decorView, msg, Snackbar.LENGTH_SHORT)
+    val view = snackbar.view
+    view.findViewById<TextView>(R.id.snackbar_text).setTextColor(ContextCompat.getColor(this.requireActivity(), R.color.white))
+    snackbar.show()
 }
