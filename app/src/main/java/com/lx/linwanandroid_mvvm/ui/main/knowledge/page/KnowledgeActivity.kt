9d9c9ace21e40379c184f1b9d3407568c6504677
linxiao <linx@taxsoft.cn>
2021-07-07 16:48:17 +0800

    onRequestPermissionsResult

---
+++ app/src/main/java/com/lx/linwanandroid_mvvm/ui/main/knowledge/page/KnowledgeActivity.kt
@@ -0,0 +1,22 @@
+package com.lx.linwanandroid_mvvm.ui.main.knowledge.page
+
+import com.lx.linwanandroid_mvvm.base.BaseVMActivity
+
+/**
+ * @title：KnowledgeActivity
+ * @projectName LinWanAndroid-mvvm
+ * @description: <Description>
+ * @author linxiao
+ * @data Created in 2021/07/07
+ */
+class KnowledgeActivity: BaseVMActivity() {
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+    }
+
+    override fun startObserve() {
+    }
+}
